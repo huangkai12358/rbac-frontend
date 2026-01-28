673d8879add2fb9c85a05b4bf083f87d2213099d
huangkai <huangkai12358@gmail.com>
2026-01-28 10:55:54 +0800

    如果已经登录，那么进入"/login"自动进入"/"

--- src/router/index.ts
+++ src/router/index.ts
@@ -42,7 +42,7 @@ const router = createRouter({
     {
       path: '/:pathMatch(.*)*', // 404 页面，放最后
       component: () => import('@/views/NotFound.vue'),
-      meta: { notFound: true },
+      meta: { notFound: true }, // 是否虽然命中路由表，但是任意路径的 404 页面
     },
   ],
 })
@@ -51,16 +51,33 @@ router.beforeEach(async (to) => {
   const store = useUserStore()
   const token = localStorage.getItem('token')
 
-  // 1. 白名单（最先判断）
-  const whiteList = ['/login']
-  if (whiteList.includes(to.path)) {
+  // 1. 如果路径是 "/login"，直接放行。但如果已经登录，重定向到 "/"
+  if (to.path === '/login') {
+    if (token) {
+      return '/'
+    }
     return true
   }
 
-  // 2. 是否 404 路由
+  // // 1.5 未来可能补充白名单，包含网站静态页面，白名单直接放行
+  // const whiteList = [
+  //   '/about',
+  //   '/help',
+  // ]
+  // if (whiteList.includes(to.path)) {
+  //   return true
+  // }
+
+  // 2. 如果路径是别的
+  // - 未登录，正常路径 -> 跳 "/login"
+  // - 未登录，异常路径 -> 放行，404 路径命中
+  // - 已登录，正常路径 -> if 条件不满足，继续往下执行鉴权等步骤
+  // - 已登录，异常路径 -> if 条件不满足，继续往下执行，最终命中 404
+
+  // 是否 404 路由
   const isNotFound = to.meta.notFound === true
 
-  // 3. 未登录
+  // 未登录
   if (!token) {
     // 只有“非 404 页面”才跳 login
     if (!isNotFound) {
@@ -70,10 +87,10 @@ router.beforeEach(async (to) => {
     return true
   }
 
-  // 4. 已登录但 Pinia 没数据 → 拉 /me
-  // 只要有 token，就认为是“已登录”
-  // 但 Pinia 是内存状态，刷新会丢。
-  // 所以：如果权限列表是空的 → 说明是刷新或首次进入 → 调 /me
+  // 3. 已登录但 Pinia 没数据 → 拉 /me
+  // - 只要有 token，就认为是“已登录”
+  // - 但 Pinia 是内存状态，刷新会丢。
+  // - 所以：如果权限列表是空的 → 说明是刷新或首次进入 → 调 /me
   if (token && !store.permissions.length) {
     try {
       await store.fetchMe()
@@ -83,7 +100,7 @@ router.beforeEach(async (to) => {
     }
   }
 
-  // 5. 权限校验（必须先拉权限，再进行权限校验）
+  // 4. 权限校验（必须先拉权限，再进行权限校验）
   const requiredPermission = to.meta.permission as string | undefined
   if (requiredPermission) {
     if (!store.hasPermission(requiredPermission)) {
@@ -91,7 +108,7 @@ router.beforeEach(async (to) => {
     }
   }
 
-  // 6. 放行（不写默认返回 undefined，也会放行）
+  // 5. 放行（不写默认返回 undefined，也会放行）
   return true
 })
 
